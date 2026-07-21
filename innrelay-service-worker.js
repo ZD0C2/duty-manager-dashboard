4800cfa2446bf8590a0945d4fca6da2ed573f1d8
fady <dr.fadymagedazmy@gmail.com>
2026-07-21 01:29:59 +0100

    Implement InnRelay brand system

--- innrelay-service-worker.js
+++ innrelay-service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "innrelay-platform-v12";
+const CACHE_NAME = "innrelay-platform-v13";
 const APP_SHELL = [
   "./innrelay-start.html",
   "./innrelay-prototype.html",
@@ -17,7 +17,9 @@ const APP_SHELL = [
   "./innrelay-supabase-config.js",
   "./innrelay-qr.js?v=10",
   "./innrelay-jspdf.min.js",
-  "./innrelay-icon.svg"
+  "./innrelay-icon.svg",
+  "./innrelay-hotels-icon.svg",
+  "./innrelay-stays-icon.svg"
 ];
 
 self.addEventListener("install", (event) => {
